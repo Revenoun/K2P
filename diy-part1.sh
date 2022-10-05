76938edd193f0b071f8e8ea38e89b1e6adfd2a8f
honggang <89745297+Revenoun@users.noreply.github.com>
2022-10-05 12:33:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,4 +10,5 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
