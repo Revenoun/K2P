ce2f2568ef38f2e9c61032461941f01e983ad8ed
honggang <89745297+Revenoun@users.noreply.github.com>
2024-11-07 14:11:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,8 +11,7 @@
 #
 # 2-设置管理地址  
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate 
-# Modify default IP
-# sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
 sed -i 's/luci-theme-bootstrap/luci-theme-bootstrap/g' feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-bootstrap/g' feeds/luci/collections/luci-light/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-bootstrap/g' feeds/luci/collections/luci-nginx/Makefile
