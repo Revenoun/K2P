957e7efddba1f8dd27081cf38460da0f7698b120
honggang <89745297+Revenoun@users.noreply.github.com>
2025-08-14 14:20:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,7 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 # Add a feed source
-sed -i '2 c\src-git luci https://github.com/coolsnowwolf/luci' feeds.conf.default
-sed -i '3 c\#src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05' feeds.conf.default
+# sed -i '2 c\src-git luci https://github.com/coolsnowwolf/luci' feeds.conf.default
+# sed -i '3 c\#src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05' feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld;main' >>feeds.conf.default
 
