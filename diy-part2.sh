1f75111ae52fb8734f3ce1ccb2e13b2ae018f8f5
honggang <89745297+Revenoun@users.noreply.github.com>
2026-02-02 11:11:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,15 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+# 删除访客网络LuCI界面
+rm -rf package/feeds/luci/applications/luci-app-guest-wifi
+
+# 删除无线隔离相关包
+rm -rf package/network/services/hostapd/patches/guest-network.patch
+rm -rf package/network/services/dnsmasq/files/guest.conf
+
+# 删除防火墙隔离规则
+rm -rf package/network/firewall/files/firewall.guest
 # 2-设置管理地址  
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate 
 # 修改默认luci
