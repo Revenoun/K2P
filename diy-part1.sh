7bf5c282ce28210447b51df8fc55181f7bccb80f
honggang <89745297+Revenoun@users.noreply.github.com>
2026-09-05 18:07:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,6 +9,8 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 # Add a feed source
+# 添加 Aurora 主题源
+sed -i '$a src-git aurora https://github.com/eamonxg/luci-theme-aurora' feeds.conf.default
 # sed -i '2 c\src-git luci https://github.com/coolsnowwolf/luci' feeds.conf.default
 # sed -i '3 c\#src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05' feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
