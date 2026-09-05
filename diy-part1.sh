f3e8115193a72495b34a27fa9700296118807002
honggang <89745297+Revenoun@users.noreply.github.com>
2026-09-05 18:14:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,6 +11,10 @@
 # Add a feed source
 # 添加 Aurora 主题源
 sed -i '$a src-git aurora https://github.com/eamonxg/luci-theme-aurora' feeds.conf.default
+
+rm -rf package/luci-theme-aurora
+git clone https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
+
 # sed -i '2 c\src-git luci https://github.com/coolsnowwolf/luci' feeds.conf.default
 # sed -i '3 c\#src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05' feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
