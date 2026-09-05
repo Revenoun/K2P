93818f5d73f47216e5a4a59a99088c26a83c03c5
honggang <89745297+Revenoun@users.noreply.github.com>
2026-09-05 19:11:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,35 +1,19 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 # 2-设置管理地址  
-#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate 
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate 
 # 修改默认luci
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-ssl-nginx/Makefile
-# 3. 设置 Aurora 为 LuCI 默认主题
-#sed -i 's/bootstrap/aurora/g' package/luci/luci-base/root/etc/config/luci
 
-
-
-echo "==================== DIY-PART2 START ===================="
-cd lede
-# 修改主机名 K2P
-sed -i 's/OpenWrt/K2P/g' package/base-files/files/bin/config_generate
-# 默认LAN IP：192.168.2.1
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-# ✅修复路径，正确设置aurora为luci默认主题
-sed -i 's/bootstrap/aurora/g' package/luci/luci-base/root/etc/config/luci
-echo "==================== DIY-PART2 END ======================"
+#勾选 luci-theme-aurora
+sed -i 's/# CONFIG_PACKAGE_luci-theme-aurora is not set/CONFIG_PACKAGE_luci-theme-aurora=y/' .config
+#取消默认bootstrap主题（可选，不需要可以删掉此行）
+sed -i 's/CONFIG_PACKAGE_luci-theme-bootstrap=y/# CONFIG_PACKAGE_luci-theme-bootstrap is not set/' .config
 
 
 
