76e8eadfb56c6e679ce8855588570961ead8f9d1
honggang <89745297+Revenoun@users.noreply.github.com>
2026-09-06 10:52:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,11 +9,3 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-ssl-nginx/Makefile
-
-#勾选 luci-theme-aurora
-sed -i 's/# CONFIG_PACKAGE_luci-theme-aurora is not set/CONFIG_PACKAGE_luci-theme-aurora=y/' .config
-#取消默认bootstrap主题（可选，不需要可以删掉此行）
-sed -i 's/CONFIG_PACKAGE_luci-theme-bootstrap=y/# CONFIG_PACKAGE_luci-theme-bootstrap is not set/' .config
-
-
-
