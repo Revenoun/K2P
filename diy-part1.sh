3fd0de9c010a414c9451cd2a8548db4455333d32
honggang <89745297+Revenoun@users.noreply.github.com>
2026-09-06 10:52:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,37 +1,18 @@
 #!/bin/bash
-#=====================diy1.sh 开始=====================
-# 克隆 aurora 主题到package
-git clone https://github.com/eamonxg/luci-theme-aurora.git package/luci-theme-aurora
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
 
-# 创建uci-defaults首次开机自动设置aurora为默认主题
-mkdir -p package/luci-theme-aurora/files
-cat > package/luci-theme-aurora/files/99-set-aurora-default <<'EOF'
-#!/bin/sh
-uci set luci.main.theme='aurora'
-uci set luci.core.mediaurlbase='/luci-static/aurora'
-uci commit luci
-exit 0
-EOF
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# 将uci脚本写入Makefile，打包进固件
-echo -e "\ndefine Package/luci-theme-aurora/install\n\t\$(call Package/luci/theme/install,\$(1))\n\t\$(CP) ./files/99-set-aurora-default \$(1)/etc/uci-defaults/\nendef" >> package/luci-theme-aurora/Makefile
-
-# 更新feeds（Lean源码必须）
-./scripts/feeds update -a
-./scripts/feeds install -a
-
-# 补丁修改luci默认配置（全新刷机默认aurora）
-patch -p1 <<'EOF'
---- a/feeds/luci/modules/luci-base/root/etc/config/luci
-+++ b/feeds/luci/modules/luci-base/root/etc/config/luci
-@@ -1,6 +1,6 @@
- config core
--	option mediaurlbase '/luci-static/bootstrap'
-+	option mediaurlbase '/luci-static/aurora'
-
- config main
- 	option lang auto
--	option theme bootstrap
-+	option theme aurora
-EOF
-#=====================diy1.sh结束=====================
+# Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
