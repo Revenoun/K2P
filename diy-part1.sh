80d992821787d5a751c00121618b9d097c93963b
honggang <89745297+Revenoun@users.noreply.github.com>
2026-09-08 16:47:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+# 添加 luci-theme-aurora 主题源码
+#git clone https://github.com/eamonxg/luci-theme-aurora.git package/luci-theme-aurora
+
+
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
