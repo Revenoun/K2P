0e93abd7ef6a2632fc4eeefdcd0b69fd35dc62f2
honggang <89745297+Revenoun@users.noreply.github.com>
2026-09-12 00:02:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -5,6 +5,5 @@
 # 2-设置管理地址  
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate 
 
-# 修改 mt_wifi 驱动中的头文件引用
-sed -i 's|#include <linux/unaligned.h>|#include <asm/unaligned.h>|g' \
-  package/lean/mt/drivers/mt7615d/mt_wifi/include/os/rt_linux.h
+find package/lean/mt/drivers/mt7615d -name "rt_linux.h" -exec \
+  sed -i 's|#include <linux/unaligned.h>|#include <asm/unaligned.h>|g' {} \;
