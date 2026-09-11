38630571b8b332cdec2fc9ba08167ac02cd98ca4
honggang <89745297+Revenoun@users.noreply.github.com>
2026-09-11 19:17:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,14 +4,7 @@
 #
 # 2-设置管理地址  
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate 
-# 修改默认luci
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-ssl-nginx/Makefile
 
-# 设置 luci-theme-aurora 为默认主题
-#sed -i 's/luci-theme-bootstrap/luci-theme-aurora/g' feeds/luci/collections/luci/Makefile
 # 在 diy-part2.sh 或编译前的准备步骤中添加：
 #sed -i 's|#include <linux/unaligned.h>|#include <asm/unaligned.h>|g' package/lean/mt/drivers/mt7615d/src/mt_wifi/include/os/rt_linux.h
 
