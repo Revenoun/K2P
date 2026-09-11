25d8e9bc0b306aa61708954a1f5c3dd8ddfa7050
honggang <89745297+Revenoun@users.noreply.github.com>
2026-09-12 00:01:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -5,16 +5,6 @@
 # 2-设置管理地址  
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate 
 
-# 在 diy-part2.sh 或编译前的准备步骤中添加：
-#sed -i 's|#include <linux/unaligned.h>|#include <asm/unaligned.h>|g' package/lean/mt/drivers/mt7615d/src/mt_wifi/include/os/rt_linux.h
-
-
-# 关闭与 mt7615d 冲突的 dbdc 选项，解决编译报错
-sed -i 's/^CONFIG_PACKAGE_kmod-mt7615d_dbdc=y/# CONFIG_PACKAGE_kmod-mt7615d_dbdc is not set/' .config
-# 如果原来没有这行，直接追加关闭项
-grep -q "CONFIG_PACKAGE_kmod-mt7615d_dbdc" .config || echo "# CONFIG_PACKAGE_kmod_mt7615d_dbdc is not set" >> .config
-
-# 确保主驱动保持开启
-sed -i 's/^# CONFIG_PACKAGE_kmod-mt7615d is not set/CONFIG_PACKAGE_kmod-mt7615d=y/' .config
-grep -q "CONFIG_PACKAGE_kmod-mt7615d=" .config || echo "CONFIG_PACKAGE_kmod-mt7615d=y" >> .config
-
+# 修改 mt_wifi 驱动中的头文件引用
+sed -i 's|#include <linux/unaligned.h>|#include <asm/unaligned.h>|g' \
+  package/lean/mt/drivers/mt7615d/mt_wifi/include/os/rt_linux.h
