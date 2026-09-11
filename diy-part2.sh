c794c9f15b6d2e5de751c099580b9a4b50a17f29
honggang <89745297+Revenoun@users.noreply.github.com>
2026-09-11 21:43:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,3 +9,12 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 #sed -i 's|#include <linux/unaligned.h>|#include <asm/unaligned.h>|g' package/lean/mt/drivers/mt7615d/src/mt_wifi/include/os/rt_linux.h
 
 
+# 关闭与 mt7615d 冲突的 dbdc 选项，解决编译报错
+sed -i 's/^CONFIG_PACKAGE_kmod-mt7615d_dbdc=y/# CONFIG_PACKAGE_kmod-mt7615d_dbdc is not set/' .config
+# 如果原来没有这行，直接追加关闭项
+grep -q "CONFIG_PACKAGE_kmod-mt7615d_dbdc" .config || echo "# CONFIG_PACKAGE_kmod_mt7615d_dbdc is not set" >> .config
+
+# 确保主驱动保持开启
+sed -i 's/^# CONFIG_PACKAGE_kmod-mt7615d is not set/CONFIG_PACKAGE_kmod-mt7615d=y/' .config
+grep -q "CONFIG_PACKAGE_kmod-mt7615d=" .config || echo "CONFIG_PACKAGE_kmod-mt7615d=y" >> .config
+
