f24bf5838f709e472edc4f9944984028a02f4bff
honggang <89745297+Revenoun@users.noreply.github.com>
2026-09-12 23:22:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,8 +4,5 @@
 # 修改默认 IP 地址为 192.168.1.1 (根据自己需求修改)
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# 修改默认主机名
-#sed -i 's/OpenWrt/MyRouter/g' package/base-files/files/bin/config_generate
-
 # 修改默认主题 (这里假设使用 argon 主题，需确保源码中已包含)
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
