f8bcc9fbb6df04719091023a3a5708c3cc6ec08d
honggang <89745297+Revenoun@users.noreply.github.com>
2026-09-12 23:23:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# diy-part2.sh - 在更新 feeds 之后执行的自定义脚本
 
-# 修改默认 IP 地址（把 192.168.1.1 改成你想要的，比如 192.168.5.1）
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+# 添加第三方插件 (例如：添加 OpenClash 源码)
+# git clone --depth=1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+
+# 添加自定义软件包 (例如：替换默认的 hello world 插件版本)
+# rm -rf package/lean/luci-app-helloworld
+# git clone --depth=1 https://github.com/fw876/helloworld.git package/luci-app-helloworld
+
+# 删除不需要的默认插件以减小固件体积 (示例：删除 luci-app-xxx)
+# rm -rf package/lean/luci-app-xxx
