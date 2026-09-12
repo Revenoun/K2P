de7d5909f7d4a940ba9e465bd18099a39a0160c3
honggang <89745297+Revenoun@users.noreply.github.com>
2026-09-12 23:22:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-# 示例：添加第三方 feed（按需启用）
-# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# diy-part1.sh - 在更新 feeds 之前执行的自定义脚本
 
+# 修改默认 IP 地址为 192.168.1.1 (根据自己需求修改)
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# 修改默认主机名
+#sed -i 's/OpenWrt/MyRouter/g' package/base-files/files/bin/config_generate
+
+# 修改默认主题 (这里假设使用 argon 主题，需确保源码中已包含)
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
